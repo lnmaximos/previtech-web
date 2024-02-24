87ed3705eec704577573acab738e6634e2bba657
León Máximos <70484152+lnmaximos@users.noreply.github.com>
2024-02-24 00:48:43 -0300

    thirtieth commit

--- js/script.js
+++ js/script.js
@@ -14,19 +14,25 @@ slideBtn.addEventListener("click", () => {
 
   if (isSliding) {
     slideBtn.textContent = "Realizar previsão";
+    document.querySelector(".banner").style.transition = "left 0.6s";
   } else {
     slideBtn.textContent = "Deslizar";
+    document.querySelector(".banner").style.transition = "left 1s";
   }
 
   isSliding = !isSliding;
 });
+
+function addBorder(e) {
+  e.style.border = "1px solid rgba(255, 0, 0, 0.4)";
+}
 
 function validateInputs() {
   let isValid = true;
 
   const idade = form.elements["idade"];
   if (!idade.value.match(/^\d+$/) || parseInt(idade.value) < 16 || parseInt(idade.value) > 120) {
-    idade.style.border = "1px solid rgba(255, 0, 0, 0.4)";
+    addBorder(idade);
     isValid = false;
   }
 
@@ -44,31 +50,31 @@ function validateInputs() {
 
   const score_credito = form.elements["score_credito"];
   if (!score_credito.value.match(/^\d+$/) || parseFloat(score_credito.value) < 0 || parseFloat(score_credito.value) > 1000) {
-    score_credito.style.border = "1px solid rgba(255, 0, 0, 0.4)";
+    addBorder(score_credito);
     isValid = false;
   }
 
   const anos_de_cliente = form.elements["anos_de_cliente"];
   if (!anos_de_cliente.value.match(/^\d+$/) || parseInt(anos_de_cliente.value) < 0 || parseInt(anos_de_cliente.value) > 100) {
-    anos_de_cliente.style.border = "1px solid rgba(255, 0, 0, 0.4)";
+    addBorder(anos_de_cliente);
     isValid = false;
   }
 
   const servicos_adquiridos = form.elements["servicos_adquiridos"];
   if (!servicos_adquiridos.value.match(/^\d+$/) || parseInt(servicos_adquiridos.value) < 0 || parseInt(servicos_adquiridos.value) > 10) {
-    servicos_adquiridos.style.border = "1px solid rgba(255, 0, 0, 0.4)";
+    addBorder(servicos_adquiridos);
     isValid = false;
   }
 
   const salario_estimado = form.elements["salario_estimado"];
   if (salario_estimado.value === "" || !salario_estimado.value.match(/^\d*([\.,]?\d{0,2})?$/) || parseFloat(salario_estimado.value) < 0 || parseFloat(salario_estimado.value) > 10000000) {
-    salario_estimado.style.border = "1px solid rgba(255, 0, 0, 0.4)";
+    addBorder(salario_estimado);
     isValid = false;
   }
 
   const saldo = form.elements["saldo"];
   if (saldo.value === "" || !saldo.value.match(/^[-+]?\d*([.,]?\d{1,2})?$/) || parseFloat(saldo.value) < -10000000 || parseFloat(saldo.value) > 1000000000) {
-    saldo.style.border = "1px solid rgba(255, 0, 0, 0.4)";
+    addBorder(saldo);
     isValid = false;
   }
 
@@ -131,7 +137,7 @@ function arrow() {
   arrowRight.classList.remove("hide");
 }
 
-document.querySelector(".esvaziarBtn").addEventListener("click", () => {
+document.querySelector(".clearBtn").addEventListener("click", () => {
   arrowRight.classList.add("hide");
   document.getElementById("pais").textContent = "País";
   document.getElementById("sexo_biologico").textContent = "Sexo";
